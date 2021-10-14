9e27ac64885b79a298bc8568cc4e30714837e9f8
03difoha <hdifol.co>
2021-10-14 17:41:17 +0100

    location as data showing

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
 import { StatusBar } from "expo-status-bar";
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import MapView from "react-native-maps";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
+import * as Location from "expo-location";
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    console.log(location);
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        region={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={styles.map}
+      />
+      <Text style={styles.paragraph}>{text}</Text>
     </View>
   );
 }
