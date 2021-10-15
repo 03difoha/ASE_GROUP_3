7ff4fcf1315ee85c2aec3f885c1b92f4574cb721
03difoha <hdifol.co>
2021-10-15 16:26:56 +0100

    tidying up

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
 import MapView from "react-native-maps";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { Platform, StyleSheet, Text, View, Dimensions } from "react-native";
+import Constants from "expo-constants";
 import * as Location from "expo-location";
 
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+
   useEffect(() => {
     (async () => {
+      if (Platform.OS === "android" && !Constants.isDevice) {
+        setErrorMsg(
+          "Oops, this will not work on Snack in an Android emulator. Try it on your device!"
+        );
+        return;
+      }
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         setErrorMsg("Permission to access location was denied");
@@ -20,24 +28,27 @@ export default function App() {
     })();
   }, []);
 
+  let lat = 0;
+  let long = 0;
   let text = "Waiting..";
   if (errorMsg) {
     text = errorMsg;
   } else if (location) {
-    console.log(location);
     text = JSON.stringify(location);
+    lat = location.coords.latitude;
+    long = location.coords.longitude;
   }
 
   return (
     <View style={styles.container}>
       <MapView
-        region={{
-          latitude: location.latitude,
-          longitude: location.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
         style={styles.map}
+        region={{
+          latitude: lat,
+          longitude: long,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.05,
+        }}
       />
       <Text style={styles.paragraph}>{text}</Text>
     </View>
@@ -47,12 +58,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    padding: 20,
   },
   map: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
+  },
+  paragraph: {
+    fontSize: 12,
+    textAlign: "center",
   },
 });
