a91f249d78d84cf2a02f3b3ee62b612d3f1d2511
fravl <valentin.roettgen@gmail.com>
2021-12-05 16:32:06 +0000

    tests for format_all_prices()

--- ase-api/test_api.py
+++ ase-api/test_api.py
@@ -43,7 +43,7 @@ def get_ran_avg_prices(num):
 
 def dummy_sparql(postcodes, amounts, dates):
     """
-    dummy_sparql creates a mock output from get_prices() in app.py
+    dummy_sparql creates a mock output from get_prices() in app.py to be used as test input
 
     :param postcodes: list of strings representing postcodes
     :param amounts: list of strings representing sale prices
@@ -74,7 +74,7 @@ def dummy_sparql(postcodes, amounts, dates):
 
 def dummy_postcodes(postcodes, lats, longs):
     """
-    dummy_postcodes creates a mock output from get_postcodes() in app.py
+    dummy_postcodes creates a mock output from get_postcodes() in app.py to be used as test input
 
     :param postcodes: list of strings representing postcodes
     :param lats: list of floats representing latitudes
@@ -98,7 +98,7 @@ def dummy_postcodes(postcodes, lats, longs):
 
 def expected_output(postcodes, lats, longs, avg_prices):
     """
-    expected_output creates output in same form as format_all_prices() in app.py
+    expected_output creates output in same form as format_all_prices() in app.py to be used as expected test output
 
     :param postcodes: list of strings representing postcodes
     :param lats: list of floats representing latitudes
@@ -116,7 +116,8 @@ def expected_output(postcodes, lats, longs, avg_prices):
 
 
 def get_tests_attach_long_lat_to_prices():
-    # Partition on input length
+    # Partitioning:
+    # - input length: 0, 1, >1
     
     random.seed(3)
 
@@ -167,7 +168,10 @@ def test_attach_long_lat_to_prices(postcodes, data, expected):
 
 
 def get_tests_format_all_prices():
-    # Partition on input length and missing data/no missing data
+    # Partitioning:
+    # - input length: 0, 1, >1
+    # - missing price data: y/n
+    # - at most one sale per postcode: y/n
     
     random.seed(3)
 
@@ -179,7 +183,7 @@ def get_tests_format_all_prices():
     expected = expected_output([], [], [], [])
     tests.append((pc_data, price_data, expected))
 
-    ## Input length = 1 and no missing data
+    ## Input length = 1, no missing data, at most one sale per postcode
     input_length = 1
     pc_list = ['BN1 9RU']
     lats = get_ran_lats(input_length)
@@ -193,6 +197,43 @@ def get_tests_format_all_prices():
     expected = expected_output(pc_list, lats, longs, avg_prices)
     tests.append((pc_data, price_data, expected))
 
+    ## Input length > 1, missing data, at most one sale per postcode
+    input_length = 3
+    indeces = [0, 2] # idxs corresponding to postcodes from pc_list where there is a sale
+    pc_list = ['BN1 9RU', 'HF4 8FT', 'GGG FFF']
+    pc_list_prices = [pc_list[index] for index in indeces]
+    lats_in = get_ran_lats(input_length)
+    longs_in = get_ran_longs(input_length)
+    lats_out = [lats_in[index] for index in indeces]
+    longs_out = [longs_in[index] for index in indeces]
+    prices = ['10', '5']
+    dates = get_ran_dates(input_length)
+    avg_prices = [10, 5]
+
+    price_data = dummy_sparql(pc_list_prices, prices, dates)
+    pc_data = dummy_postcodes(pc_list, lats_in, longs_in)["data"]
+    expected = expected_output(pc_list_prices, lats_out, longs_out, avg_prices)
+    tests.append((pc_data, price_data, expected))
+
+
+    ## input length > 1, no missing data, more than one sale per postcode
+    input_length = 3
+    data_length = 6 # sparql returns 6 bindings
+    indeces = [0, 0, 0, 1, 1, 2] # idxs corresponding to sales in a postcode
+    pc_list = ['BN1 9RU', 'HF4 8FT', 'GGG FFF']
+    pc_list_prices = [pc_list[index] for index in indeces]
+    lats = get_ran_lats(input_length)
+    longs = get_ran_longs(input_length)
+    
+    prices = ['10', '5', '3', '20', '10', '99']
+    dates = get_ran_dates(data_length)
+    avg_prices = [6, 15, 99]
+
+    price_data = dummy_sparql(pc_list_prices, prices, dates)
+    pc_data = dummy_postcodes(pc_list, lats, longs)["data"]
+    expected = expected_output(pc_list, lats, longs, avg_prices)
+    tests.append((pc_data, price_data, expected))
+
     return tests
 
 @pytest.mark.parametrize("postcodes,price_data,expected", get_tests_format_all_prices())
@@ -201,4 +242,11 @@ def test_format_all_prices(postcodes, price_data, expected):
     assert res == expected
 
 
+def get_tests_format_prices_by_year():
+    # Partitioning:
+    # - input length: 0, 1, >1
+    # - missing price data: y/n
+    # - at most one sale per postcode: y/n
+    tests = []
 
+    return tests
