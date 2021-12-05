b2e42b66372d6bbaa57bb8fe4633f8f18fb34d37
fravl <valentin.roettgen@gmail.com>
2021-12-05 23:20:54 +0000

    fix format_prices_by_year ignoring first datapoint per postcode

--- ase-api/app.py
+++ ase-api/app.py
@@ -58,15 +58,15 @@ def format_prices_by_year(postcodes, price_data):
         if p["postcode"]["value"] not in res:
             # if no record of postcode in res, then create a new empty record
             res[p["postcode"]["value"]] = {"lat": 0, "long": 0, 'years': {}}
+        
+        # if a record for this postcode exists, but not for this year then create an array with the current sale value in
+        if p['date']['value'][0:4] not in res[p["postcode"]["value"]]['years']:
+            res[p["postcode"]["value"]]['years'][p['date']
+                                                    ['value'][0:4]] = [int(p['amount']['value'])]
         else:
-            # if a record for this postcode exists, but not for this year then create an array with the current sale value in
-            if p['date']['value'][0:4] not in res[p["postcode"]["value"]]['years']:
-                res[p["postcode"]["value"]]['years'][p['date']
-                                                     ['value'][0:4]] = [int(p['amount']['value'])]
-            else:
-                # if we do have a record for this year already then append the current sale value onto the year array
-                res[p["postcode"]["value"]]['years'][p['date']['value'][0:4]].append(
-                    int(p['amount']['value']))
+            # if we do have a record for this year already then append the current sale value onto the year array
+            res[p["postcode"]["value"]]['years'][p['date']['value'][0:4]].append(
+                int(p['amount']['value']))
 
     res = attach_long_lat_to_prices(postcodes.json()['result'], res)
     return res
