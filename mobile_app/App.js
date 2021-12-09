df0c439f01024e3810c19850a3abf2c381ff245f
Piyush Agrawal <reachpiyushagrawal@gmail.com>
2021-12-09 18:24:15 +0530

    Added average price display on marker description
    Cleaned the code a bit and the average price is displayed when the location marker is clicked.

--- mobile_app/App.js
+++ mobile_app/App.js
@@ -5,8 +5,6 @@ import * as Device from "expo-device";
 import * as Network from "expo-network";
 import Geocoder from 'react-native-geocoder';
 import Geocode from "react-geocode";
-
-
 import {
   Platform,
   StyleSheet,
@@ -34,17 +32,12 @@ export default function App() {
   const [click_flag, setClick_Flag] = useState(false);
   const [postcode_keyboardfocus_flag, setPostCode_KeyboardFocus_Flag] = useState(false);
   const [latlongclick, setLatLongClick] = useState({ latitude: lat, longitude: long});
-
-  //console.log(hm_points)
   const [errorMsg, setErrorMsg] = useState("");
   const [internetReachable, setInternetReachable] = useState(null);
   const [isAppTime, setAppTime] = useState(false);
   const [postCodeMode, setPostCodeMode] = useState(false);
-  
-  
   var [postCodeInput, setPostCodeInput] = useState("");
   var tempInput = ''
-
   const ref = useRef(null);
 
   useEffect(() => {
@@ -64,62 +57,33 @@ export default function App() {
   }, []);  
 
   async function update_latlong_loc() {
- 
     update_hm_points()
-
     console.log('curr lat long (from Location): ', location)
-    //setHM_Points([{'latitude' : 51.5072178, 'longitude' : -0.12758619999999998 , 'weight' : 1}, {'latitude' : 51.5072176, 'longitude' : -0.12758612 , 'weight' : 1}])
+    }
  
-    }
-
-  
   async function update_latlong_post() {
-
-    
     update_hm_points_post()
-    
-
     console.log('curr lat long (from postcode): ', lat, long)
-    
     }
-
 
   async function update_latlong_click(e) {
-
     console.log('heyehgeyhyey', '', e.nativeEvent.coordinate["latitude"])
-
     setLat(e.nativeEvent.coordinate["latitude"]);
     setLong(e.nativeEvent.coordinate["longitude"]);
-
     console.log('curr lat long (from click): ', lat, long)
-
      }
 
-
-
 async function update_hm_points() {
-
-
-    
     function neaten_the_data_to_the_format_specified(dirty_hm_points) {
-
       if (dirty_hm_points['message'] != "Internal Server Error") {
-
-      let clean_hm_points = Object.values(dirty_hm_points).map((i) => ({'latitude' : Object.values(i)[0], 'longitude' : Object.values(i)[1], 'weight' : Object.values(i)[2]}));   //([key, value]) => {lat : {value.lat} long : {value.long} weight : {value.avg_price}})
-
-      //console.log('After Mapping: ', clean_hm_points);
-      //let clean_hm_points = [{'latitude' : 51.5072178, 'longitutde' : -0.12758619999999998 , 'weight' : 1}]
+      let clean_hm_points = Object.values(dirty_hm_points).map((i) => ({'latitude' : Object.values(i)[0], 'longitude' : Object.values(i)[1], 'weight' : Object.values(i)[2]}));
       setHM_Points(clean_hm_points);
-      
       console.log('sending lat long ##################### Succesffuly sent : ', lat, long)
       }
-   
     }
-    
+
     async function send_location(lat, long) {
-
       console.log('sending lat long ##################### does send even run? : ', lat, long)
-
       fetch("https://b274zqubga.execute-api.us-east-1.amazonaws.com/dev/", {   
         method: "POST", // or 'PUT'
         headers: {
@@ -132,58 +96,34 @@ async function update_hm_points() {
           //console.log("Success:", data)
           console.log('sending lat long ##################### just about 2 send : ', lat, long)
           neaten_the_data_to_the_format_specified(data)
-          
-          //var dataa = data
-    
-          //return dataa
-    
-          
         })
         .catch((error) => {
           console.error("Error:", error);
-          //return null
+
         });
     }
-
 
     console.log('sending lat long ##################### pre-sending : ', lat, long)
     let location = await Location.getCurrentPositionAsync({});
-
     setLat(location.coords.latitude);
     setLong(location.coords.longitude);
     setLocation(location);
-
     let lat_loc = location.coords.latitude
     let long_loc = location.coords.longitude
-    
     send_location(lat_loc, long_loc);
-    
   }
 
-
   async function update_hm_points_click(e) {
-
-
-    
     function neaten_the_data_to_the_format_specified(dirty_hm_points) {
-
       if (dirty_hm_points['message'] != "Internal Server Error") {
-
-      let clean_hm_points = Object.values(dirty_hm_points).map((i) => ({'latitude' : Object.values(i)[0], 'longitude' : Object.values(i)[1], 'weight' : Object.values(i)[2]}));   //([key, value]) => {lat : {value.lat} long : {value.long} weight : {value.avg_price}})
-
-      //console.log('After Mapping: ', clean_hm_points);
-      //let clean_hm_points = [{'latitude' : 51.5072178, 'longitutde' : -0.12758619999999998 , 'weight' : 1}]
+      let clean_hm_points = Object.values(dirty_hm_points).map((i) => ({'latitude' : Object.values(i)[0], 'longitude' : Object.values(i)[1], 'weight' : Object.values(i)[2]}));
       setHM_Points(clean_hm_points);
-      
       console.log('sending lat long ##################### Succesffuly sent : ', lat, long)
       }
-   
     }
     
     async function send_location(lat, long) {
-
       console.log('sending lat long ##################### does send even run? : ', lat, long)
-
       fetch("https://b274zqubga.execute-api.us-east-1.amazonaws.com/dev/", {   
         method: "POST", // or 'PUT'
         headers: {
@@ -196,55 +136,29 @@ async function update_hm_points() {
           //console.log("Success:", data)
           console.log('sending lat long ##################### just about 2 send : ', lat, long)
           neaten_the_data_to_the_format_specified(data)
-          
-          //var dataa = data
-    
-          //return dataa
-    
-          
         })
         .catch((error) => {
           console.error("Error:", error);
-          //return null
         });
     }
-
-
     let lat_click = e.nativeEvent.coordinate["latitude"];
     let long_click = e.nativeEvent.coordinate["longitude"];
-
     console.log('sending lat long ##################### pre-sending : ', lat, long)
-
-    
-
-    
     send_location(lat_click, long_click);
     
   }
 
   async function update_hm_points_post() {
-
-
-    
     function neaten_the_data_to_the_format_specified(dirty_hm_points) {
-
       if (dirty_hm_points['message'] != "Internal Server Error") {
-
-      let clean_hm_points = Object.values(dirty_hm_points).map((i) => ({'latitude' : Object.values(i)[0], 'longitude' : Object.values(i)[1], 'weight' : Object.values(i)[2]}));   //([key, value]) => {lat : {value.lat} long : {value.long} weight : {value.avg_price}})
-
-      //console.log('After Mapping: ', clean_hm_points);
-      //let clean_hm_points = [{'latitude' : 51.5072178, 'longitutde' : -0.12758619999999998 , 'weight' : 1}]
+      let clean_hm_points = Object.values(dirty_hm_points).map((i) => ({'latitude' : Object.values(i)[0], 'longitude' : Object.values(i)[1], 'weight' : Object.values(i)[2]}));
       setHM_Points(clean_hm_points);
-      
       console.log('sending lat long ##################### Succesffuly sent : ', lat, long)
       }
-   
     }
     
     async function send_location(lat, long) {
-
       console.log('sending lat long ##################### does send even run? : ', lat, long)
-
       fetch("https://b274zqubga.execute-api.us-east-1.amazonaws.com/dev/", {   
         method: "POST", // or 'PUT'
         headers: {
@@ -257,35 +171,21 @@ async function update_hm_points() {
           //console.log("Success:", data)
           console.log('sending lat long ##################### just about 2 send : ', lat, long)
           neaten_the_data_to_the_format_specified(data)
-          
-          //var dataa = data
-    
-          //return dataa
-    
-          
+
         })
         .catch((error) => {
           console.error("Error:", error);
           //return null
         });
     }
-
-
-
     let postcode_location = await Location.geocodeAsync(postCodeInput);
-
     setLat(postcode_location[0]['latitude']);
     setLong(postcode_location[0]['longitude']);
-
     let lat_post = postcode_location[0]['latitude']
     let long_post = postcode_location[0]['longitude']
-
-    
     send_location(lat_post, long_post);
     
   }
-
-
 
   function clickToMove(e) {console.log('click', latlongclick)
                           console.log(e.nativeEvent.coordinate["latitude"])
@@ -304,12 +204,7 @@ async function update_hm_points() {
           latitudeDelta: 0.04,
           longitudeDelta: 0.05,
         }}
-
-        onPress={(e) =>  {clickToMove(e)}} // latitude: lat, longitude: long})}
-
-      >
-
-      
+        onPress={(e) =>  {clickToMove(e)}}>
         <MapView.Heatmap
           points={hm_points}
           opacity={0.6}
@@ -324,134 +219,80 @@ async function update_hm_points() {
             latitude: lat,
             longitude: long,
           }}
-          title={"Your Current Location"}
+          title={"Average price in this area is £" + hm_points[0].weight}
+          //title={JSON.stringify(hm_points)}
         />
         {markers.map((marker, index) => (
           <MapView.Marker key={index} coordinate={marker} />
         ))}  
       </MapView>
-
-
       <Text style={styles.paragraph}>{errorMsg}</Text>
-          
-
       <View flex = {0}
-      flexDirection = 'row'
-      paddingBottom = {15}
-      //width = {200}
-      >
-
+        flexDirection = 'row'
+        paddingBottom = {15}>
       </View>
-
       <BackButton />
-      
     </View>
   }
 
   function enter() {setAppTime(true), console.log('hello>?'), update_latlong_loc()}
-
-  
   function EnterButton(props) {
     return (
-  
-      <Button 
+    <Button 
       title = 'Use my location'
       onPress =  {() => enter()} >
-       
     </Button>
-         
     );
   }
 
   function BackButton(props) {
     return (
-  
-      <Button 
+    <Button 
       title = 'Back Now'
       onPress =  {() => setAppTime(false)} >
-       
     </Button>
-         
     );
   }
   function PostClickcheck(text) {
-        
     setPostCodeInput(text)
-
     postCodeInput = text
-   
     Keyboard.dismiss()
-
     setAppTime(true)
-
     update_latlong_post()
-
   }
 
   function PostCodeButton(props) {
     return (
-
-      <Button 
+    <Button 
       title = 'Enter A Postode'
       onPress =  {() => PostClickcheck(tempInput)} >
-       
-    </Button>
-         
+    </Button>   
     );
   }
-
-  
 
   function setTempin(text) {
     tempInput = text
   }
 
-
   function IntroPage() {
-  
     return <View style={[styles.text_stuff, styles.container]}>
-  
       <Text style={[styles.text_stuff]}>
-        
-        Hey there, 
-
-          Would you like to use your phone's current location or enter a postcode?
-          
+        "Hey there, Would you like to use your phone's current location or enter a postcode?"
         </Text> 
-
         <Text> 
-
         </ Text>
-  
         <EnterButton />
-
         <Text> 
-
         </ Text>
-
         <PostCodeButton />
-
         <Text> 
-
         </ Text>
-
         <TextInput  style={styles.text_input} 
-
-                    onChangeText = {(text) => setTempin(text)}
-                      
+                    onChangeText = {(text) => setTempin(text)} 
                     onSubmitEditing = {() => PostClickcheck(tempInput)}  >
-                       </TextInput>
-
-
-
-  
+       </TextInput>
        </View>
-
-       
   
-    
-  
-      
   }
   
   function Greeting(props) {
@@ -462,11 +303,6 @@ async function update_hm_points() {
     }
     return <IntroPage />;
   }
-  
-
-
-
-
   return (    
     <Greeting isAppTime = {isAppTime} />
     );
